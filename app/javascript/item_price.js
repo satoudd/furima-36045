2e8a8a0825f0e8c22467eae2483352fbed77f148
satoudd <88134639+satoudd@users.noreply.github.com>
2021-12-04 17:42:27 +0900

    v

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,11 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    const addTaxDom = document.getElementById("profit");
-    addTaxDom.innerHTML = "price-input*0.1="
+    const addTaxDom = document.getElementById("add-tax-price");
+    const honmask = document.getElementById("profit")
+    var tax = inputValue*0.1;
+    var price = inputValue-tax;
+    addTaxDom.innerHTML = tax;
+    honmask.innerHTML = price;
 })
 })
