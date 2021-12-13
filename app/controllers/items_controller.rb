4216361f5918cdb4c728c46ffba696304c423c59
satoudd <88134639+satoudd@users.noreply.github.com>
2021-12-13 16:32:25 +0900

    k

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit]
+  before_action :move_to_index, except: [:index, :show, :new, :create, :update, :destroy]
 
   def new
     @item = Item.new
@@ -22,10 +24,37 @@ def show
   @item = Item.find(params[:id])
 end
 
+def edit
+
+  unless user_signed_in? && current_user == @item.user
+    redirect_to action: :index
+  end
+  @item = Item.find(params[:id])
+end
+
+def update
+  @item = Item.find(params[:id])
+ if @item.update(item_params)
+  redirect_to root_path
+else
+  render :edit
+end
+
+end
+
   private
 
   def item_params
     params.require(:item).permit(:product_name, :image, :prices, :product_description, :category_id, :product_id, :shippingcharge_id, :shipping_area_id, :days_to_ship_id).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user == @item.user
+      redirect_to action: :index
+    end
+  end
 end
